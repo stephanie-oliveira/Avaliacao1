08244d3a47eee45eb08234e309c929f4c771aca7
Stephanie Oliveira <stephanie.oliveira@estudante.sc.senai.br>
2017-03-13 14:18:29 -0300

    Commit inicial

---
+++ Source/Avaliacao1/Avaliacao1GameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Avaliacao1.h"
+#include "Avaliacao1GameMode.h"
+
+
+
+
---
+++ Source/Avaliacao1/Avaliacao1GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Avaliacao1GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AVALIACAO1_API AAvaliacao1GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
